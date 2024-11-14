7df5d6e1edb8213ae61f9dfc886755d975e7b4be
jeong-yeahzee <yezi920921@naver.com>
2024-11-14 22:28:55 +0900

    db table 설계

--- local_db.js
+++ local_db.js
@@ -1,20 +1,10 @@
 import {db} from "./main.js";
 
-export function connect_db(){
-    const table_obj = {
-        "SHOP_T": "",
-        "BRAND_T": "CREATE TABLE IF NOT EXISTS BRAND_T ()",
-        "PRODUCT_T": "CREATE TABLE IF NOT EXISTS PRODUCT_T ()",
-        "PRODUCT_DC_T": "CREATE TABLE IF NOT EXISTS PRODUCT_DC_T ()",
-        "ORDER_TABLE": "CREATE TABLE IF NOT EXISTS ORDER_TABLE ()",
-        "PAYMENT_TABLE": "CREATE TABLE IF NOT EXISTS PAYMENT_TABLE ()"
-    }
-
-    table_obj.SHOP_T = `CREATE TABLE IF NOT EXISTS SHOP_T (
+const shop_query = `CREATE TABLE IF NOT EXISTS SHOP_T (
                           SHOP_NO INTEGER PRIMARY KEY AUTOINCREMENT,
-                          SHOP_NAME TEXT,
-                          STATUS TEXT DEFAULT 1,
-                          BUSINESS_LICENSE TEXT,
+                          SHOP_NAME TEXT NOT NULL,
+                          STATUS INTEGER NOT NULL DEFAULT 1,
+                          BUSINESS_LICENSE TEXT NOT NULL,
                           TEL TEXT,
                           MOBILE TEXT,
                           EMAIL TEXT,
@@ -23,18 +13,88 @@ export function connect_db(){
                           ZIPCODE TEXT,
                           CEO_NAME TEXT,
                           MEMO TEXT,
-                          FIRST_CREATE_DT TEXT,
-                          LAST_UPDATE_DT TEXT,
-                          UNIQUE(SHOP_NO)
+                          FIRST_CREATE_DT TEXT NOT NULL,
+                          LAST_UPDATE_DT TEXT NOT NULL
                         );`;
-    table_obj.BRAND_T = `CREATE TABLE IF NOT EXISTS BRAND_T (
+
+const brand_query = `CREATE TABLE IF NOT EXISTS BRAND_T (
                           BRAND_NO INTEGER PRIMARY KEY AUTOINCREMENT,
-                          BRAND_NAME TEXT,
-                          STATUS TEXT DEFAULT 1,
+                          BRAND_NAME TEXT NOT NULL,
+                          STATUS INTEGER NOT NULL DEFAULT 1,
                           ORDER_NO INTEGER,
                           MEMO TEXT,
-                          FIRST_CREATE_DT TEXT,
-                          LAST_UPDATE_DT TEXT,
-                          UNIQUE(BRAND_NO)
+                          FIRST_CREATE_DT TEXT NOT NULL,
+                          LAST_UPDATE_DT TEXT NOT NULL
                         );`;
+
+const product_query = `CREATE TABLE IF NOT EXISTS PRODUCT_T (
+                        BRAND_NO INTEGER,
+                        PRODUCT_NO INTEGER PRIMARY KEY AUTOINCREMENT,
+                        PRODUCT_NAME TEXT NOT NULL,
+                        MEMO TEXT,
+                        PRICE_IN INTEGER NOT NULL DEFAULT 0,
+                        PRICE_OUT INTEGER NOT NULL DEFAULT 0,
+                        ORDER_NO TEXT,
+                        STATUS INTEGER NOT NULL DEFAULT 1,
+                        FIRST_CREATE_DT TEXT NOT NULL,
+                        LAST_UPDATE_DT TEXT NOT NULL
+                        );`;
+const dc_query = `CREATE TABLE IF NOT EXISTS PRODUCT_DC_T (
+                        BRAND_NO INTEGER NOT NULL,
+                        PRODUCT_NO INTEGER NOT NULL,
+                        SHOP_NO INTEGER NOT NULL,
+                        DISCOUNT_PERCENT INTEGER NOT NULL DEFAULT 0,
+                        DISCOUNT_PRICE INTEGER NOT NULL DEFAULT 0,
+                        STATUS INTEGER NOT NULL DEFAULT 0,
+                        FIRST_CREATE_DT TEXT NOT NULL,
+                        LAST_UPDATE_DT TEXT NOT NULL
+                        PRIMARY KEY (BRAND_NO,PRODUCT_NO,SHOP_NO)
+                        );`;
+
+const sales_query = `CREATE TABLE IF NOT EXISTS SALES_T (
+                        MASTER_NO INTEGER NOT NULL,
+                        SALES_NO INTEGER PRIMARY KEY AUTOINCREMENT,
+                        SHOP_NO INTEGER NOT NULL,
+                        SHOP_NAME TEXT NOT NULL,
+                        BRAND_NO INTEGER NOT NULL,
+                        BRAND_NAME TEXT NOT NULL,
+                        PRODUCT_NO INTEGER NOT NULL,
+                        PRODUCT_NAME TEXT NOT NULL,
+                        SALES_COUNT INTEGER NOT NULL,
+                        DISCOUNT_PERCENT INTEGER NOT NULL DEFAULT 0,
+                        DISCOUNT_PRICE INTEGER NOT NULL DEFAULT 0,
+                        SALES_PRICE_OUT INTEGER NOT NULL,
+                        SALES_DC_PRICE_OUT INTEGER NOT NULL,
+                        TOTAL_SALES_PRICE_OUT INTEGER NOT NULL,
+                        TOTAL_SALES_DC_PRICE_OUT INTEGER NOT NULL,
+                        SALES_TYPE INTEGER NOT NULL DEFAULT 1,
+                        SALES_DT TEXT NOT NULL,
+                        FIRST_CREATE_DT TEXT NOT NULL,
+                        LAST_UPDATE_DT TEXT NOT NULL
+                        );`;
+
+const payment_query = `CREATE TABLE IF NOT EXISTS PAYMENT_T (
+                        PAY_NO INTEGER PRIMARY KEY AUTOINCREMENT,
+                        PAY_TYPE INTEGER NOT NULL DEFAULT 1,
+                        SHOP_NO INTEGER NOT NULL,
+                        CASH_AMOUNT INTEGER,
+                        CARD_AMOUNT INTEGER,
+                        DISCOUNT_AMOUNT INTEGER,
+                        PAYMENT_AMOUNT INTEGER,
+                        ADMIN_AMOUNT INTEGER,
+                        MEMO TEXT,
+                        PAY_DT TEXT NOT NULL,
+                        FIRST_CREATE_DT TEXT NOT NULL,
+                        LAST_UPDATE_DT TEXT NOT NULL
+                        );`;
+
+export function connect_db(){
+    const table_obj = {
+        "SHOP_T": shop_query,
+        "BRAND_T": brand_query,
+        "PRODUCT_T": product_query,
+        "PRODUCT_DC_T": dc_query,
+        "SALES_T": sales_query,
+        "PAYMENT_T": payment_query
+    };
 }
